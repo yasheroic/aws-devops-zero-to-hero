64b84077da7f74b485f5f08e162817dbd12c9dec
Yash Shaw <yshaw2002@gmail.com>
2025-11-12 03:04:44 +0530

    changed docker port

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull yasheroic/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 yasheroic/simple-python-flask-app
+docker run -d -p 8000:8000 yasheroic/simple-python-flask-app
